1bb50beb480e39dc59d5243769b16f53f07d6abe
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-08-04 17:55:07 +0700

    Update Commit

--- config.php
+++ config.php
@@ -2,6 +2,8 @@
 
 include("/var/www/mysql-config2.php");
 
+$mydatabase = $IS_DEVELOPMENT ? "gazillionairedev" : "gazillionaire";
+
 $url_sandbox = "https://sandbox.itunes.apple.com/verifyReceipt";
 $url_production = "https://buy.itunes.apple.com/verifyReceipt";
 
