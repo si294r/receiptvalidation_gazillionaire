0bbab4e11411ea85201d806f1068600526eb1bb7
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-08-04 20:05:45 +0700

    Update Commit

--- validate.php
+++ validate.php
@@ -29,34 +29,26 @@ $array_json = json_decode($result, TRUE);
 if ($array_json["status"] == 0) {
     if (in_array($product_id, array_column($array_json["receipt"]["in_app"], "product_id"))) {
         
-        // TODO check database 
-        $already_purchase = false;
         $connection = new PDO(
             "mysql:dbname=$mydatabase;host=$myhost;port=$myport",
             $myuser, $mypass
         );
         
+        $arr_transaction_id = [];
         foreach ($array_json["receipt"]["in_app"] as $k=>$v) {
             if ($v['product_id'] == $product_id) {
-                
-                $sql = "SELECT * FROM transactions WHERE transaction_id = :transaction_id "
-                        . "AND NOW() <= COALESCE(expired_date, NOW())";
-        
-                $statement1 = $connection->prepare($sql);
-                $statement1->bindParam(":transaction_id", $v['transaction_id']);
-                $statement1->execute();
-                $row = $statement1->fetch(PDO::FETCH_ASSOC);
-                
-                var_dump($v['transaction_id']);
-                var_dump($row);
-                if ($row) {
-                    $already_purchase = true;
-                    break;
-                }
-                
+                $arr_transaction_id[] = $v['transaction_id'];                
             }
         }
-        if ($already_purchase) { 
+        $sql = "SELECT * FROM transactions WHERE transaction_id IN ('" . implode("','", $arr_transaction_id) . "') "
+                . "AND NOW() <= COALESCE(expired_date, NOW())";
+        $statement1 = $connection->prepare($sql);
+        $statement1->execute();
+        $row = $statement1->fetch(PDO::FETCH_ASSOC);
+
+//        TODO - define expired_date still manual
+        var_dump($row);
+        if ($row) {
             $response = array("error" => 1, "message" => $product_id . " already purchase");
         } else {
             $response = array("error" => 0, "message" => "");
