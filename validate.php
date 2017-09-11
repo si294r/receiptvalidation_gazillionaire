e0ae1ca8783fbe5ebaac4f904631714b89ad034f
Chairuddinsyah <chairuddinsyah@10.0.4.13>
2017-09-11 18:26:30 +0700

    Update Commit

--- validate.php
+++ validate.php
@@ -29,6 +29,28 @@ $array_json = json_decode($result, TRUE);
 if ($array_json["status"] == 0) {
     if (in_array($product_id, array_column($array_json["receipt"]["in_app"], "product_id"))) {
         
+        if ($IS_DEVELOPMENT == false) {
+            $filter_time = "NOW() <= COALESCE(expired_date, NOW())"; 
+        } else {
+            $iservice = "gettime-dev";
+            $result_gettime = file_get_contents('http://alegrium5.alegrium.com/gazillionaire/cloudsave/?'.$iservice, null, stream_context_create(
+                    array(
+                        'http' => array(
+                            'method' => 'POST',
+                            'header' => 'Content-Type: application/json'. "\r\n"
+                            . 'x-api-key: ' . X_API_KEY_TOKEN . "\r\n"
+                            . 'Content-Length: ' . strlen('{}') . "\r\n",
+                            'content' => '{}'
+                        )
+                    )
+                )
+            );
+            $result_gettime = json_decode($result_gettime, true);
+            $timestamp = $result_gettime['timestamp'];
+
+            $filter_time = "$timestamp <= COALESCE(UNIX_TIMESTAMP(expired_date), $timestamp)"; 
+        }
+
         $connection = new PDO(
             "mysql:dbname=$mydatabase;host=$myhost;port=$myport",
             $myuser, $mypass
@@ -42,7 +64,7 @@ if ($array_json["status"] == 0) {
         }
         $sql = "SELECT transaction_id FROM transactions "
                 . "WHERE transaction_id IN ('" . implode("','", $arr_transaction_id) . "') "
-                . "AND NOW() <= COALESCE(expired_date, NOW())";
+                . "AND $filter_time";
         $statement1 = $connection->prepare($sql);
         $statement1->execute();
         $row = $statement1->fetch(PDO::FETCH_ASSOC);
